462f397f6a2621d28c6db219e70e44d6677e1791
glyve99 <guilhermemolyveira@hotmail.com>
2020-11-24 22:05:57 -0300

    Alterando nome de arquivo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import AutoFin from './components/pages/AutoFin';
-import Regex from './components/pages/regex';
+import Regex from './components/pages/Regex';
 
 function App() {
   return (
