0a337291015f8432fb0bfef550c10f90b1f7019d
glyve99 <guilhermemolyveira@hotmail.com>
2021-01-15 00:21:48 -0300

    Mudando estilo automatos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
-import AutoFin from './components/pages/AutoFin';
 import Regex from './components/pages/Regex';
 import Gramatica from './components/pages/Gramatica';
-import Test from './components/pages/Test';
-import Test2 from './components/pages/Test2';
-import Test3 from './components/pages/Test3';
+import AutoFin from './components/pages/AutoFin';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={Home} />
-        <Route path='/autofin' component={AutoFin} />
         <Route path='/gramatica' component={Gramatica} />
         <Route path='/regex' component={Regex} />
-        <Route path='/test' component={Test} />
-        <Route path='/test2' component={Test2} />
-        <Route path='/test3' component={Test3} />
+        <Route path='/autofin' component={AutoFin} />
       </Switch>
     </BrowserRouter>
   );
